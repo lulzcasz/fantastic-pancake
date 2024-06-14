eff395f1697c58525cae471f40db377e2f95bb9f
lulzcasz <reisdossousalucasedson@gmail.com>
2024-06-14 18:26:36 -0300

    refactor: move import inside create_app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
-from .blog import blog as blog_bp
-
 def create_app():
     app = Flask(__name__)
 
+    from .blog import blog as blog_bp
     app.register_blueprint(blog_bp)
 
     return app
