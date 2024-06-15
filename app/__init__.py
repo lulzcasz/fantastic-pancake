b21e6e4574e5d6f958c8f6a7fdbdea6151506d31
lulzcasz <reisdossousalucasedson@gmail.com>
2024-06-15 02:23:52 -0300

    refactor: create blueprints folder

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from .blog import blog as blog_bp
+    from .blueprints import blog as blog_bp
     app.register_blueprint(blog_bp)
 
     return app
