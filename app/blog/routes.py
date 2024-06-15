fa09e2ce4229c6d43380adae5cc8dda73e5b07a9
lulzcasz <reisdossousalucasedson@gmail.com>
2024-06-15 01:07:08 -0300

    feat: create simple example using HTMX

--- app/blog/routes.py
+++ app/blog/routes.py
@@ -1,7 +1,13 @@
-from flask import render_template
+from flask import render_template, request, abort
 
 from . import blog
 
-@blog.route('')
+@blog.route('', methods = ['GET'])
 def index():
     return render_template('blog/index.html')
+
+@blog.route('world', methods = ['GET'])
+def world():
+    if not 'HX-Request' in request.headers: return abort(404)
+
+    return render_template('blog/partials/world.html')
